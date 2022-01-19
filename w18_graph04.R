87a3df8d0a80ecaec975a0f97fdc48975a0eca37
jhchou97 <90920762+jhchou97@users.noreply.github.com>
2022-01-19 15:16:02 +0800

    Add files via upload

---
+++ w18_graph04.R
@@ -0,0 +1,56 @@
+setwd("D:/3_NTPU/110-1-data-visualization/110-1-course")
+library(tidyverse)
+library(RColorBrewer)
+####################################################
+#  電信信令人口統計之建置、分析與應用(內政部2020)  #
+####################################################
+mp <- econDV2::Map()
+mp$sf$get_sf_taiwan_simplified() ->
+  sf_taiwan_simplified
+
+econDV2::Object(demo)
+demo$df <- read_csv("2020NOV_district.csv")
+demo$df |>
+  mutate(訊號量 = NIGHT_WORK - NIGHT_WEEKEND) %>%
+  group_by(TOWN)-> demo$df
+
+
+Left <- sf_taiwan_simplified$台灣本島$鄉鎮區
+Right <- demo$df
+  
+
+demo$join <-{
+  Left %>%
+    right_join(
+      Right, by = c("name" = "TOWN")
+    )
+}
+
+
+demo$background$台灣本島$鄉鎮區 <- {
+  sf_taiwan_simplified$台灣本島$鄉鎮區 |>
+    sf::st_cast("MULTIPOLYGON") |>
+    mp$sf$make_background_map(
+      color="white",
+      size=0.1
+    )+theme_void()
+}
+
+demo$plot <- function(){
+  demo$background$台灣本島$鄉鎮區+geom_sf(
+      data = demo$join,
+      mapping = aes(
+        fill = 訊號量
+      ),
+      color = "white",
+      size = 0.01
+  )+
+    colorspace::scale_fill_continuous_sequential(
+      palette="reds", 
+      na.value="grey70"
+    )+
+    labs(title = "週間與週末之夜間訊號量差異",
+         subtitle = "週間指週一到週五/ 單位：人", # 補充單位說明
+         caption = "資料來源：內政部統計處
+       109年11月行政區電信信令人口統計")
+  }
